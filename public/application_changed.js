c470fecf1d20605dddbeda1b9b009be56b4edd16
Anthony Lai <aslai@MacBook-Pro.local>
2009-11-29 20:51:20 -0800

    Added my changes to application_changed.js

---
+++ public/application_changed.js
@@ -0,0 +1,69 @@
+var log = console.log;
+var mb, wc;
+
+// Main WebCenter resource module
+var webCenter = function(){
+  var resourceIndexURL = "/rest/api/resourceIndex";
+  var resourceIndex = null;
+
+  var getResourceIndex = function(callback){
+    if(!resourceIndex) {
+      $.getJSON(resourceIndexURL, theCallback(callback));
+    } else {
+	theCallback(callback)();
+    }
+  };
+  var theCallback = function(callback) {
+      resourceIndex = data;
+      callback();
+  }
+  var getResourceURL = function(urn, callback) {
+      getResourceIndex(theCallback2(callback));
+  };
+
+  var theCallback2 = function(callback) {
+    var resourceURL = $.grep(resourceIndex.links, function(n){
+        return n.resourceType == urn;
+      })[0].href;
+    callback();
+    return resourceURL;
+
+  }
+
+  //getResourceIndex();
+
+  return {
+    resourceIndex : getResourceIndex,
+    getResourceURL : getResourceURL
+  }
+};
+
+// MessageBoard module
+var msgBoard = function(wc, resURL) {
+  var wcInstance = wc;
+  var resourceURL = null;
+  var defaultResourceURL = 'urn:oracle:webcenter:messageBoard';
+  var getEntries = function(callback){
+    if (!resourceURL) {
+      wc.getResourceURL(resourceURL, theCallback(callback));
+    } else {
+	theCallback(callback);
+    }
+  }
+  var theCallback = function(callback) {
+    $.getJSON(resourceURL, callback);
+  }
+
+  return {
+    resourceURL: function(){return resourceURL},
+    defaultResourceURL: function() { defaultResourceURL },
+    getMessages: getEntries
+  }
+};
+
+$(function(){
+  wc = webCenter();
+  mb = msgBoard(wc, 'urn:oracle:webcenter:messageBoard');
+  mb.getEntries(function(data){console.log(data)});
+  
+});
