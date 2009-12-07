841a6fb04237bf6348ba24493fbee5cc4e441860
Rich Manalang <rich.manalang@gmail.com>
2009-12-07 12:05:31 -0800

    update

--- js/application.js
+++ js/application.js
@@ -57,7 +57,7 @@ $(function(){
          })
       };
       console.log(JSON.stringify(bindData));
-      $('option.list-filter-option').autoRender(JSON.stringify(bindData));
+      $('option.list-filter-option').autoRender(bindData);
       });
 
     // Infinite scroll pager
