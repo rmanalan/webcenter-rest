388dc5c564f79dfe23ca1433ab8b616cd9eaa920
Rich Manalang <rich.manalang@gmail.com>
2009-12-16 15:26:16 -0800

    customizing spaces css

--- js/ir.js
+++ js/ir.js
@@ -1,1 +1,10 @@
-alert('woo hoo!');
+;(
+  var assetHost = 'http://aconmt01.us.oracle.com/';
+  $.xLazyLoader({
+    'css' : $.map(['ir.css'],function(n){return assetHost + 'css/' + n;}
+  });
+)();
+
+/* 
+vim:ts=2:sw=2:expandtab
+*/
