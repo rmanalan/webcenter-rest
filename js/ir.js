f24dcf7adadd24edf66ed1f9835e4d2bdd943098
Rich Manalang <rich.manalang@gmail.com>
2009-12-16 15:26:59 -0800

    customizing spaces css

--- js/ir.js
+++ js/ir.js
@@ -1,9 +1,7 @@
-;(
-  var assetHost = 'http://aconmt01.us.oracle.com/';
-  $.xLazyLoader({
-    'css' : $.map(['ir.css'],function(n){return assetHost + 'css/' + n;}
-  });
-)();
+var assetHost = 'http://aconmt01.us.oracle.com/';
+$.xLazyLoader({
+  'css' : $.map(['ir.css'],function(n){return assetHost + 'css/' + n;}
+});
 
 /* 
 vim:ts=2:sw=2:expandtab
