f4eb4bfd1c827dfb1da6dd4126b9ab58b69a977c
Rich Manalang <rich.manalang@gmail.com>
2009-12-23 11:47:16 -0800

    html

--- js/ir.js
+++ js/ir.js
@@ -4,17 +4,27 @@ $.xLazyLoader({
   'css' : $.map(['ir.css'],function(n){return assetHost + 'css/' + n;}),
   'js' : [assetHost + 'js/pure.js', assetHost + 'js/wcutils.js', assetHost + 'js/wcrest.js']
 });
+/*
 $(function(){
   var gs = $('#group-switcher');
-  gs.html('<ul id="gs"><li><a href="/webcenter/spaces/home">Home</a></li></ul>');
+  gs.html('<ul id="gs"><li class="gsspace"><a class="gsspacelink" href="/webcenter/spaces/home">Home</a></li></ul>');
   webCenter.init({'port':apiPort},function(){
     console.log(currentUser);
     currentUser.getSpaces(function(){
       console.log(currentUser.spaces);
+      var data = {
+         'gsspace' : $.map(currentUser.spaces, function(n){
+           return {
+             'gsspacelink' : n.name,
+             'gsspacelink@href' : 
+           }
+         })
+      }
     });
   });
 
 });
+*/
 /* 
 vim:ts=2:sw=2:expandtab
 */
