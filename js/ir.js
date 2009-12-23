fb88521d665f620be33c47dbb69d6fde816cf530
Rich Manalang <rich.manalang@gmail.com>
2009-12-23 11:31:28 -0800

    webCenter.init

--- js/ir.js
+++ js/ir.js
@@ -1,8 +1,19 @@
 var assetHost = 'http://aconmt01.us.oracle.com/';
-$.xLazyLoader({
-  'css' : $.map(['ir.css'],function(n){return assetHost + 'css/' + n;})
+var apiPort = 8892;
+jQuery.noConflict();
+jQuery.xLazyLoader({
+  'css' : $.map(['ir.css'],function(n){return assetHost + 'css/' + n;}),
+  'js' : [assetHost + 'js/pure.js', assetHost + 'js/wcutils.js', assetHost + 'js/wcrest.js']
 });
+jQuery(function(){
+  webCenter.init({'port':apiPort},function(){
+    console.log(currentUser);
+    currentUser.getSpaces(function(){
+      console.log(currentUser.spaces);
+    });
+  });
 
+});
 /* 
 vim:ts=2:sw=2:expandtab
 */
