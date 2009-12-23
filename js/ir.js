decf1ee98655233ee7c00c3aeb4aa33b097557ed
Rich Manalang <rich.manalang@gmail.com>
2009-12-23 14:04:02 -0800

    set/get cached spaces

--- js/ir.js
+++ js/ir.js
@@ -8,7 +8,7 @@ $(function(){
   var gs = $('#group-switcher');
   gs.html('<select id="gs"><option class="gsspace spacename url@value" value="/webcenter/spaces/home">Home</option></select>');
   webCenter.init({'port':apiPort},function(){
-    var spacesCached = $.DOMCached.get('groups');
+    var spacesCached = $.DOMCached.get('groups','webcenter');
     if(spacesCached){
       $('.gsspace').clone(true).appendTo('#gs').autoRender(spacesCached);
       $('#gs').bind('change',function(){
