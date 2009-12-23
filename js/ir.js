a144e8b75291067494793e35573051082996254b
Rich Manalang <rich.manalang@gmail.com>
2009-12-23 11:33:11 -0800

    webCenter.init

--- js/ir.js
+++ js/ir.js
@@ -1,11 +1,10 @@
 var assetHost = 'http://aconmt01.us.oracle.com/';
 var apiPort = 8892;
-jQuery.noConflict();
-jQuery.xLazyLoader({
-  'css' : jQuery.map(['ir.css'],function(n){return assetHost + 'css/' + n;}),
+$.xLazyLoader({
+  'css' : $.map(['ir.css'],function(n){return assetHost + 'css/' + n;}),
   'js' : [assetHost + 'js/pure.js', assetHost + 'js/wcutils.js', assetHost + 'js/wcrest.js']
 });
-jQuery(function(){
+$(function(){
   webCenter.init({'port':apiPort},function(){
     console.log(currentUser);
     currentUser.getSpaces(function(){
