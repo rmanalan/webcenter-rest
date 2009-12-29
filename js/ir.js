879b8ba8adddc4f6eed61b318a4f8fd141d1aec3
Anthony Lai <aslai@MacBook-Pro.local>
2009-12-28 16:06:37 -0800

    Testing select box

--- js/ir.js
+++ js/ir.js
@@ -8,9 +8,9 @@ $(function(){
   var gs = $('#group-switcher');
   gs.html('<select id="gs" onchange="location=this.value"><option class="gsspace spacename url@value selected@selected" value="/webcenter/spaces/home">Home</option></select><a href="/webcenter/faces/oracle/webcenter/community/view/pages/manage/ManageSpaces-SpacesTab.jspx">Browse Group Space</a>');
   var spacesCached = $.DOMCached.get('groups','webcenter');
-  if(spacesCached){
-    $('.gsspace').clone(true).appendTo('#gs').autoRender(spacesCached);
-  }
+//  if(spacesCached){
+//    $('.gsspace').clone(true).appendTo('#gs').autoRender(spacesCached);
+//  }
   webCenter.init({'port':apiPort},function(){
     currentUser.getSpaces(function(){
       var bindData = {
@@ -27,7 +27,7 @@ $(function(){
 	bindData += { 'spacename':'Home test', 'url':'/webcenter/faces/oracle/webcenter/community/view/pages/manage/ManageSpaces-SpacesTab.jsp' };
 	bindData += { 'spacename':'Create Group Space', 'url':'/webcenter/faces/oracle/webcenter/community/view/pages/manage/ManageSpaces-SpacesTab.jspx' };
       $.DOMCached.set('groups',bindData,86400,'webcenter');
-      if(spacesCached) return;
+//      if(spacesCached) return;
       $('.gsspace').clone(true).appendTo('#gs').autoRender(bindData);
     });
   });
