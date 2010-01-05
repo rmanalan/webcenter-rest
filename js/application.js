2cc748fd272bd4bcdabceb771cac521e2dea2fc3
Rich Manalang <rich.manalang@gmail.com>
2010-01-05 14:27:37 -0800

    changed port and hostname

--- js/application.js
+++ js/application.js
@@ -2,7 +2,7 @@ $(document).ajaxStart(function(){$('#loading-ind').show()}).ajaxStop(function(){
 
 $(function(){
   function initApp(callback){
-    webCenter.init({'port':8894,'hostname':'dadvmi0286.us.oracle.com'},function(){
+    webCenter.init({'port':apiPort},function(){
       // show who's logged in
       $('ul.headnav li').autoRender({
         'username' : currentUser.name.formatted,
