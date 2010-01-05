09c63922eed9f0d368eeab006285a0832159eff7
Anthony Lai <aslai@dhcp-whq-wifi-west-10-151-4-173.usdhcp.oraclecorp.com>
2010-01-05 10:40:59 -0800

    Made home and browse group working now, caching disabled

--- js/ir.js
+++ js/ir.js
@@ -1,34 +1,58 @@
 var assetHost = 'http://aconmt01.us.oracle.com/';
-var apiPort = 8892;
+//var apiPort = 8893;
 $.xLazyLoader({
   'css' : $.map(['dd','ir'],function(n){return assetHost+'css/'+n+'.css'}),
   'js' : $.map(['pure','json','domcached','dd','wcutils','wcrest'],function(n){return assetHost+'js/'+n+'.js'})
 });
 $(function(){
   var gs = $('#group-switcher');
-  gs.html('<select id="gs" onchange="location=this.value"><option class="gsspace spacename url@value selected@selected" value="/webcenter/spaces/home">Home</option></select><a href="/webcenter/faces/oracle/webcenter/community/view/pages/manage/ManageSpaces-SpacesTab.jspx">Browse Group Space</a>');
+  gs.html('<select id="gs" onchange="location=this.value"><option class="gsspace spacename url@value selected1@selected" value="/webcenter/spaces/home">Home</option></select>');
   var spacesCached = $.DOMCached.get('groups','webcenter');
 //  if(spacesCached){
-//    $('.gsspace').clone(true).appendTo('#gs').autoRender(spacesCached);
+//      $('.gsspace').autoRender(bindData);
+////    $('.gsspace').clone(true).appendTo('#gs').autoRender(spacesCached);
 //  }
-  webCenter.init({'port':apiPort},function(){
+//  alert('here2');
+  webCenter.init({},function(){
     currentUser.getSpaces(function(){
       var bindData = {
          'gsspace' : $.map(currentUser.spaces, function(n){
            return {
              'spacename' : n.displayName,
+             'name' : n.name,
              'url' : '/webcenter/spaces/' + n.name,
-             'selected' : function(){
+             'selected1' : function(){
                
              }
            }
          })
       };
-	bindData += { 'spacename':'Home test', 'url':'/webcenter/faces/oracle/webcenter/community/view/pages/manage/ManageSpaces-SpacesTab.jsp' };
-	bindData += { 'spacename':'Create Group Space', 'url':'/webcenter/faces/oracle/webcenter/community/view/pages/manage/ManageSpaces-SpacesTab.jspx' };
+	var test1 = 'test123';
+////	alert(bindData['gsspace']);
+////	alert(bindData['gsspace'][0].spacename);
+////	alert(bindData['gsspace'].length);
+	bindData['gsspace'].unshift({'name':'', 'spacename':'Home', 'url':'/webcenter/faces/oracle/webcenter/community/view/pages/manage/ManageSpaces-SpacesTab.jspx'});
+	bindData['gsspace'].push({'spacename':'Browse Group Space', 'url':'/webcenter/faces/oracle/webcenter/community/view/pages/manage/ManageSpaces-SpacesTab.jspx' });
+	bindData['gsspace'][2].selected1 = 'selected';
+////	alert(bindData['gsspace'][2].spacename);
+////	alert(bindData['gsspace'][2].selected1);
+
+////	alert('here: ' + currentGroupSpace);
+////      alert(bindData);
       $.DOMCached.set('groups',bindData,86400,'webcenter');
 //      if(spacesCached) return;
-      $('.gsspace').clone(true).appendTo('#gs').autoRender(bindData);
+//      $('.gsspace').clone(true).appendTo('#gs').autoRender(bindData);
+      $('.gsspace').autoRender(bindData);
+      for (index in bindData['gsspace']) {
+////        alert(bindData['gsspace'][index].name);
+////	alert(i + space[name] + currentGroupSpace);
+////	   $('#gs option')[2].selected = true;
+	if (bindData['gsspace'][index].name == currentGroupSpace) {
+//	   alert('true');
+	   $('#gs option')[index].selected = true;
+        }
+      }
+////      $('#gs option')[2].selected = true;
     });
   });
 
