a4935429315d22bbbfe21cec0ff96d28b9922028
Rich Manalang <rich.manalang@gmail.com>
2010-01-26 13:11:03 -0800

    updated css

--- js/ir.js
+++ js/ir.js
@@ -8,11 +8,10 @@ $(function(){
   var gs = $('#group-switcher');
   gs.html('<select id="gs" onchange="location=this.value"><option class="gsspace spacename url@value selected1@selected" value="/webcenter/spaces/home">Home</option></select>');
   var spacesCached = $.DOMCached.get('groups','webcenter');
-//  if(spacesCached){
-//      $('.gsspace').autoRender(bindData);
-////    $('.gsspace').clone(true).appendTo('#gs').autoRender(spacesCached);
-//  }
-//  alert('here2');
+  if(spacesCached){
+    $('.gsspace').autoRender(bindData);
+    $('.gsspace').clone(true).appendTo('#gs').autoRender(spacesCached);
+  }
   webCenter.init({},function(){
     currentUser.getSpaces(function(){
       var bindData = {
@@ -22,37 +21,25 @@ $(function(){
              'name' : n.name,
              'url' : '/webcenter/spaces/' + n.name,
              'selected1' : function(){
-               
              }
            }
          })
       };
 	var test1 = 'test123';
-////	alert(bindData['gsspace']);
-////	alert(bindData['gsspace'][0].spacename);
-////	alert(bindData['gsspace'].length);
-        bindData['gsspace'].unshift({'name':'', 'spacename':'Home', 'url':'/webcenter/spaces/home'});
+  bindData['gsspace'].unshift({'name':'', 'spacename':'Home', 'url':'/webcenter/spaces/home'});
 	bindData['gsspace'].push({'spacename':'Browse Group Space', 'url':'/webcenter/faces/oracle/webcenter/community/view/pages/manage/ManageSpaces-SpacesTab.jspx' });
 	bindData['gsspace'][2].selected1 = 'selected';
-////	alert(bindData['gsspace'][2].spacename);
-////	alert(bindData['gsspace'][2].selected1);
-
-////	alert('here: ' + currentGroupSpace);
-////      alert(bindData);
       $.DOMCached.set('groups',bindData,86400,'webcenter');
-//      if(spacesCached) return;
-//      $('.gsspace').clone(true).appendTo('#gs').autoRender(bindData);
+      if(spacesCached) return;
+      $('.gsspace').clone(true).appendTo('#gs').autoRender(bindData);
       $('.gsspace').autoRender(bindData);
       for (index in bindData['gsspace']) {
-////        alert(bindData['gsspace'][index].name);
-////	alert(i + space[name] + currentGroupSpace);
-////	   $('#gs option')[2].selected = true;
-	if (bindData['gsspace'][index].name == currentGroupSpace) {
-//	   alert('true');
-	   $('#gs option')[index].selected = true;
+        $('#gs option')[2].selected = true;
+        if (bindData['gsspace'][index].name == currentGroupSpace) {
+	        $('#gs option')[index].selected = true;
         }
       }
-////      $('#gs option')[2].selected = true;
+      $('#gs option')[2].selected = true;
     });
   });
 
