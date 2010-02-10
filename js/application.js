158338cae1bba9f7d60b1ea81117b167ad335332
Rich Manalang <rich.manalang@gmail.com>
2010-02-10 14:21:53 -0800

    rounded corners for IE

--- js/application.js
+++ js/application.js
@@ -32,7 +32,7 @@ $(function(){
       .blur(function(){if(this.value.replace(' ')=='')$(this).val(placeholder)});
   }
   if($.browser.msie) {
-    $('#publisher, #pub1-text textarea, #share').corner({          
+    $('#publisher').corner({          
       tl: { radius: 5 },
       tr: { radius: 5 },
       bl: { radius: 5 },
