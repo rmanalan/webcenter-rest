7763428ebcb20c181c2d41bf2191b71fa96a6f35
Rich Manalang <rich.manalang@gmail.com>
2010-04-20 11:23:32 -0700

    attempt to make preview slides larger

--- js/application.js
+++ js/application.js
@@ -218,7 +218,7 @@
 										thumbsBorderOpacity: .7,
 										leftButtonInner: '&laquo;',
 										rightButtonInner: '&raquo;',
-										thumbsPercentReduction: 50
+										thumbsPercentReduction: 45
 									});
 								});
 							};
